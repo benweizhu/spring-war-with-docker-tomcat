b7fbb32ad208d6730cebb7fcc75516b64951701b
benweizhu <benweizhu@gmail.com>
2017-06-23 13:43:48 +0800

    make it pure spring boot run with container

--- src/main/java/me/zeph/springboot/Application.java
+++ src/main/java/me/zeph/springboot/Application.java
@@ -1,9 +1,10 @@
+package me.zeph.springboot;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
-
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
     }
